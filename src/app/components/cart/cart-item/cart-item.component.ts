0416a6c471e21bbddd561c126947a37be1b4c627
AKapran8 <kapranandriyu@gmail.com>
2021-03-13 22:01:54 +0200

    start cart-item

--- src/app/components/cart/cart-item/cart-item.component.ts
+++ src/app/components/cart/cart-item/cart-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
+import { IProduct } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-cart-item',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartItemComponent implements OnInit {
 
-  constructor() { }
+  list: any[] = [];
+  sum: number = 0;
+  // Test 15 line add service
+  constructor(public productService: ProductsService) { }
 
   ngOnInit(): void {
+    this.productService.productCartSubject.subscribe(product => {
+      this.list.push(product);
+      this.sum = this.list.reduce((acc, sum) => acc += sum.price)
+    })
   }
 
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -10,19 +10,26 @@ import { IProduct, IGetProductResponse } from '../interfaces/product';
 })
 export class ProductsService {
 
+
   cartSubject = new Subject<IProduct>();
+  count: number = 0;
+  // TEST 17 line
+  productCartSubject = new Subject<IProduct>();
 
   constructor(private http: HttpClient) { }
 
-
-
-
+  // Отримуєм продукти
   getProducts(): Observable<IGetProductResponse> {
     return this.http.get<IGetProductResponse>('https://nodejs-final-mysql.herokuapp.com/products?keyword=&pageNumber=1');
   }
 
+
   buy(product: IProduct) {
+    if (product) this.count++;
+    // Відправляєм збільшення к-ті в корзині
     this.cartSubject.next(product);
+    // Test Відправляєм данні продукта
+    this.productCartSubject.next(product);
   }
 
 }
